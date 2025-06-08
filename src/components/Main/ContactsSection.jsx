8de343db8e5285f16aea287630393ccbe9eb2092
Velislav Shopov <shopov.velislav@gmail.com>
2025-06-08 19:38:37 +0300

    Changed some a tags

--- src/components/Main/ContactsSection.jsx
+++ src/components/Main/ContactsSection.jsx
@@ -9,7 +9,7 @@ export default function ContactsSection() {
     <section id="contacts" className={classes.contacts_section}>
       <h2 className={classes.contacts_title}>Contacts</h2>
       <div className={classes.img_flex_container}>
-        <a href="https://www.facebook.com/velislav.shopov.10" target="_blank">
+        <a href="https://www.facebook.com/velislav.shopov.10">
           <img
             src={facebook_img}
             className={classes.contact_img}
@@ -19,14 +19,14 @@ export default function ContactsSection() {
         <a href="mailto:shopov.velislav@gmail.com">
           <img src={gmail_img} className={classes.contact_img} alt="Mail"></img>
         </a>
-        <a href="https://www.instagram.com/v_shopov20/" target="_blank">
+        <a href="https://www.instagram.com/v_shopov20/">
           <img
             src={instagram_img}
             className={classes.contact_img}
             alt="Instagram"
           ></img>
         </a>
-        <a href="https://github.com/VelislavShopov" target="_blank">
+        <a href="https://github.com/VelislavShopov">
           <img
             src={github_img}
             className={classes.contact_img}
