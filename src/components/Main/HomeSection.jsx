f1e409ff32644a045a12c5b5cf3eb61cb969f1a5
Velislav Shopov <shopov.velislav@gmail.com>
2025-06-08 19:37:57 +0300

    added alt to profile img

--- src/components/Main/HomeSection.jsx
+++ src/components/Main/HomeSection.jsx
@@ -4,7 +4,7 @@ import classes from "./HomeSection.module.css";
 export default function HomeSection() {
   return (
     <section id="home" className={classes.home_section}>
-      <img src={pic} className={classes.img}></img>
+      <img src={pic} className={classes.img} alt="Profile pic"></img>
       <div className={classes.title_container}>
         <h1 className={classes.intro_p}>Hello, I'm Velislav!</h1>
         <p className={classes.second_title}>Software Engineering Student</p>
